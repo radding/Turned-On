5d3e60f9ce014decc2f2aeb3004eb5322bd04149
Adam Kenney <akenney@pmd.com>
2015-02-07 17:55:58 -0500

    First draft of Python endpoint for sending verification code via Twilio.

--- app/views.py
+++ app/views.py
@@ -11,6 +11,28 @@ def home(request):
     """Renders the home page."""
    return JsonRequest({"text":"hi"})
 
+def sendSmsVerificationCode(request):
+	from twilio.rest import TwilioRestClient
+
+	fromNumber = "+14012065509"
+	ACCOUNT_SID = "ACf3f0805e01bc0a3db41e7aae79bc96d5"
+	AUTH_TOKEN = "acf544c7ffb70d7b888eabc81d75698a"
+
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+	verificationCode = "1234"
+	userPhoneNumberToVerify = "2063838296"
+
+	client.messages.create(
+		to=userPhoneNumberToVerify,
+		from_=fromNumber,
+		body="Here is your TurnedOn verification code: " + verificationCode,
+	)
+
+	response = HttpResponse()
+	response.status_code = 200
+	return response
+
 def contact(request):
     """Renders the contact page."""
     assert isinstance(request, HttpRequest)
