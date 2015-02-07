219d3583831d269d64cd18225bac129da59f0af6
Yoseph Radding <raddingy@lykeme.com>
2015-02-07 17:25:37 -0500

    Fixed things

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ Definition of views.
 """
 
 from django.shortcuts import render
-from django.http import HttpRequest JsonRequest
+from django.http import HttpRequest, JsonRequest
 from django.template import RequestContext
 from datetime import datetime
 
